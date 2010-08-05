c3dd11d6430534d3a9c6f39de5e138a65169b9ad
apgwoz <git@apgwoz.com>
2010-08-05 10:41:44 -0400

    * adding markdown, vcl (varnish), basic java support

---
+++ activator.d/22java.el
@@ -0,0 +1,11 @@
+;; I don't like tabs, but Java programmers sure do!
+
+(add-hook 'java-mode-hook 
+          '(lambda () 
+             (progn
+               (flyspell-mode t)
+               (c-set-style "Ellemtel")
+               (set (make-local-variable 'c-basic-offset) 3)
+               (set (make-local-variable 'tab-width) 3)
+               (set (make-local-variable 'show-trailing-whitespace) t)
+               (set (make-local-variable 'indent-tabs-mode) t))))
