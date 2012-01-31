0bed9c756a689cdb2aac735651c7a2667453a387
Andrew Gwozdziewycz <apg@apg.office.okcupid.com>
2012-01-31 10:12:01 -0500

    * add coffee-mode

--- activator.d/17js.el
+++ activator.d/17js.el
@@ -1,7 +1,12 @@
 ;; requires
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/coffee-mode"))
+(require 'coffee-mode)
+
 (autoload 'js2-mode "js2" nil t)
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile$" . coffee-mode))
 
 (add-hook 'js2-mode-hook 
           '(lambda ()
@@ -11,3 +16,7 @@
       js2-indent-on-enter-key t
       js2-mode-escape-quotes t
       js2-basic-offset 2)
+
+(add-hook 'coffee-mode-hook
+          '(lambda ()
+             (set (make-local-variable 'tab-width) 2)))
--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 (setq activator-load-path (expand-file-name "~/.emacs.d/activator.d/"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/"))
 
+;; thingy.
+(setq x-super-keysym 'meta)
+
 (require 'activator)
 (activator-start)
 (put 'narrow-to-region 'disabled nil)
