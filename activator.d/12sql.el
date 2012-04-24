469df6421b59ff69587221e309f8aef5f3115088
Andrew Gwozdziewycz <git@apgwoz.com>
2012-04-24 11:25:37 -0400

    * add a local binding for sql-send-region-and-switch

--- activator.d/12sql.el
+++ activator.d/12sql.el
@@ -1,7 +1,14 @@
 (add-to-list 'auto-mode-alist '("\\.ddl$" . sql-mode))
 (add-to-list 'auto-mode-alist '("\\.sql$" . sql-mode))
 
+(defun sql-send-region-and-switch (beg end)
+  (interactive "r")
+  (progn
+    (sql-send-region beg end)
+    (switch-to-buffer-other-window sql-buffer)))
+
 (add-hook 'sql-mode-hook 
           '(lambda ()
+             (local-set-key "\C-x \C-e" 'sql-send-region-and-switch)
              (flyspell-prog-mode)))
 
