22289df1601dead1a14a04cbf75bdc8c87dceed3
Andrew Gwozdziewycz <git@apgwoz.com>
2012-07-05 15:20:59 -0400

    * customize go formatting

--- activator.d/14go.el
+++ activator.d/14go.el
@@ -1,1 +1,7 @@
 (require 'go-mode-load)
+
+(add-hook 'go-mode-hook
+          (lambda ()
+            (set (make-local-variable 'c-basic-offset) 2)
+            (set (make-local-variable 'tab-width) 2)
+            (set (make-local-variable 'show-trailing-whitespace) t)))
