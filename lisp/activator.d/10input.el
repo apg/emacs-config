30813e7f80bfdb7fa74be7c62f5a7c05836ca6da
Andrew G10z <agwozdziewycz@salesforce.com>
2019-04-01 12:35:22 -0700

    Everyone gets a scrabble tile.

--- lisp/activator.d/10input.el
+++ lisp/activator.d/10input.el
@@ -1,2 +1,17 @@
 ;;; because technomancy laughed at me.
 (defun shrug () (interactive) (insert "¯\\_(ツ)_/¯"))
+
+(defun scrabble-emoji (begin end)
+  "Silly slack fun for scrabble tiling text."
+  (interactive "r")
+  (let ((s (buffer-substring begin end)))
+    (with-temp-buffer
+      (insert (apply #'concat
+                     (seq-map '(lambda (ch)
+                                 (cond
+                                  ((and (>= ch 97)
+                                        (<= ch 122))
+                                   (format ":scrabble-%c:" ch))
+                                  (t (char-to-string ch))))
+                              (downcase s))))
+      (kill-region (point-min) (point-max)))))
