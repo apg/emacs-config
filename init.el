d918e9d48025a6d9fb87e8b896eb9387a6f41c14
andrew.gwozdziewycz@strongdm.com <andrew.gwozdziewycz@HW-0287.lan>
2024-07-16 14:26:52 -0700

    Require the thing to set the var.

--- init.el
+++ init.el
@@ -50,6 +50,7 @@
 ;;; import env
 
 (when (memq window-system '(mac ns x))
+  (require 'exec-path-from-shell)
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "GITHUB_TOKEN"))
     (add-to-list 'exec-path-from-shell-variables var))
   (exec-path-from-shell-initialize))
